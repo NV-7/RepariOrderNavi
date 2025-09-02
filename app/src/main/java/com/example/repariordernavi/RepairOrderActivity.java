f508ae296bc908c82c4890d4db7d4662db942e3f
Tommy <naviboun@gmail.com>
2025-09-02 12:18:14 -0500

    Created Java Input for total

--- app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
+++ app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repariordernavi;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity {
 
+    Double number = 0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Random gen = new Random();
+        double ranNum = gen.nextDouble();
+        TextView totalTV = findViewById(R.id.totalInput);
+
+        totalTV.setText((int) ranNum);
+
+        String value = totalTV.getText().toString();
+
+        Log.i("TEST", value);
+
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
