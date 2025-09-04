6a49b6bbb14742c8cd5a20fcedf203e945906c2d
Tommy <naviboun@gmail.com>
2025-09-04 12:05:39 -0500

    Added the ability to take user input and submit

--- app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
+++ app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,19 +18,17 @@ import java.util.Random;
 
 public class RepairOrderActivity extends AppCompatActivity {
 
-    Double number = 0.0;
+    TextView subtotalTV;
+    TextView taxTV;
+    TextView totalTV;
     Button submitButton;
-    View.OnClickListener buttonListner = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Random gen = new Random();
-            double ranNum = gen.nextDouble();
-            TextView totalTV = findViewById(R.id.totalInput);
-            totalTV.setText((String.valueOf(ranNum)));
-            Toast.makeText(RepairOrderActivity.this, "Submitted", Toast.LENGTH_SHORT).show();
+    EditText orderET;
+    EditText technitianET;
+    EditText laborET;
+    EditText partET;
+    EditText paintET;
+    EditText inspectionET;
 
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +38,16 @@ public class RepairOrderActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
 
-        Random gen = new Random();
-        double ranNum = gen.nextDouble();
-        TextView totalTV = findViewById(R.id.totalInput);
+        totalTV = findViewById(R.id.totalInput);
+        subtotalTV = findViewById(R.id.subtotalInput);
+        taxTV = findViewById(R.id.taxinput);
+        orderET = findViewById(R.id.orderInput);
+        technitianET = findViewById(R.id.technititanInput);
+        laborET = findViewById(R.id.laborInput);
+        partET = findViewById(R.id.partInput);
+        paintET = findViewById(R.id.paintInput);
+        inspectionET = findViewById(R.id.inspectioninput);
+
        // totalTV.setText((String.valueOf(ranNum)));
         submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(buttonListner);
@@ -55,4 +61,42 @@ public class RepairOrderActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    View.OnClickListener buttonListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Random gen = new Random();
+            double tax = 100*gen.nextDouble();
+            double subtotal = 100*gen.nextDouble();
+            double total = tax + subtotal;
+
+//            totalTV = findViewById(R.id.totalInput);
+//            subtotalTV = findViewById(R.id.subtotalInput);
+//            taxTV = findViewById(R.id.taxinput);
+//            orderET = findViewById(R.id.orderInput);
+//            technitianET = findViewById(R.id.technititanInput);
+//            laborET = findViewById(R.id.laborInput);
+//            partET = findViewById(R.id.partInput);
+//            paintET = findViewById(R.id.paintInput);
+
+
+            String orderVal = orderET.getText().toString();
+            String technitianVal = technitianET.getText().toString();
+            String laborVal = laborET.getText().toString();
+            String partVal = partET.getText().toString();
+            String paintVal = paintET.getText().toString();
+            String inspectionVal = inspectionET.getText().toString();
+
+            totalTV.setText(String.valueOf(total));
+            subtotalTV.setText((String.valueOf(subtotal)));
+            taxTV.setText((String.valueOf(tax)));
+
+
+
+            taxTV = findViewById(R.id.taxinput);
+          //  Toast.makeText(RepairOrderActivity.this, "Submitted", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RepairOrderActivity.this, partVal , Toast.LENGTH_SHORT).show();
+        }
+    };
 }
