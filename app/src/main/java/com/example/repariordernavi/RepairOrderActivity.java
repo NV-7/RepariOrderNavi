a20da49099624e119ed1de870937b52d490c2a8b
Tommy <naviboun@gmail.com>
2025-09-04 11:28:53 -0500

    Created submit button and anonymous button lister to change the total. Also added a toast to specify a submit

--- app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
+++ app/src/main/java/com/example/repariordernavi/RepairOrderActivity.java
@@ -2,7 +2,10 @@ package com.example.repariordernavi;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,23 +18,37 @@ import java.util.Random;
 public class RepairOrderActivity extends AppCompatActivity {
 
     Double number = 0.0;
+    Button submitButton;
+    View.OnClickListener buttonListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Random gen = new Random();
+            double ranNum = gen.nextDouble();
+            TextView totalTV = findViewById(R.id.totalInput);
+            totalTV.setText((String.valueOf(ranNum)));
+            Toast.makeText(RepairOrderActivity.this, "Submitted", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Random gen = new Random();
-        double ranNum = gen.nextDouble();
-        TextView totalTV = findViewById(R.id.totalInput);
-
-        totalTV.setText((int) ranNum);
-
-        String value = totalTV.getText().toString();
-
-        Log.i("TEST", value);
 
         super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
+
+        Random gen = new Random();
+        double ranNum = gen.nextDouble();
+        TextView totalTV = findViewById(R.id.totalInput);
+       // totalTV.setText((String.valueOf(ranNum)));
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(buttonListner);
+        String value = totalTV.getText().toString();
+
+
+        Log.i("TEST", value);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
